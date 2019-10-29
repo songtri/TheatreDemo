6f2e5b12631594f8cd968c8cdce34066c120ffd8
Magtibay <57116048+Magtibay@users.noreply.github.com>
2019-10-28 20:21:49 -0400

    Add files via upload

--- Theatre.cs
+++ Theatre.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+//michael magtibay 301029090
 namespace TheatreDemo
 {
 	class Theatre
@@ -13,34 +15,81 @@ namespace TheatreDemo
 
 		public Theatre(string name)
 		{
-		}
+            this.Name = name;
+            this.shows = new List<Show>();
+
+        }
 
 		public void AddShow(Show show)
 		{
+            this.shows.Add(show);
 		}
 
 		public void PrintShows()
 		{
-		}
+
+            foreach (var show in shows)
+            {
+                Console.WriteLine(show);
+            }
+        }
 
 		public void PrintShows(Genre genre)
 		{
+            foreach (var show in shows)
+            {
+                if (show.Movie.Genre.HasFlag(genre))
+                {
+                    Console.WriteLine(show);
+                }
+
+            }
+
+          
 		}
 
 		public void PrintShows(Day day)
 		{
-		}
+
+            var Shows = shows.FindAll(d => d.Day == day);
+            foreach (var show in Shows)
+            {
+               
+                Console.WriteLine(show);
+            }
+        }
 
 		public void PrintShows(Time time)
 		{
-		}
+            var Shows = shows.FindAll(t => t.Time == time);
+            foreach (var show in Shows)
+            {
+
+                Console.WriteLine(show);
+            }
+
+        }
 
 		public void PrintShows(string actor)
 		{
-		}
+            var Shows = shows.FindAll(a => a.Movie.Cast.Contains(actor));
+            foreach (var show in Shows)
+            {
+
+                Console.WriteLine(show);
+            }
+
+        }
 
 		public void PrintShows(Day day, Time time)
 		{
-		}
+            var Shows = shows.FindAll(s => s.Time == time && s.Day == day);
+            foreach (var show in Shows)
+            {
+
+                Console.WriteLine(show);
+            }
+
+        }
 	}
 }
