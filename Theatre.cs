569a668033350d1f636dde0f41cc4b63fe6be382
욱 송 <songtri@gmail.com>
2019-10-28 20:44:46 -0400

    Removed unnecessary indentation and lines, changed spaces to tabs

--- Theatre.cs
+++ Theatre.cs
@@ -1,95 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 
 //michael magtibay 301029090
 namespace TheatreDemo
 {
 	class Theatre
 	{
-		private List<Show> shows = new List<Show>();
+		private List<Show> shows;
 		public string Name { get; }
 
 		public Theatre(string name)
 		{
-            this.Name = name;
-            this.shows = new List<Show>();
-
-        }
+			Name = name;
+			shows = new List<Show>();
+		}
 
 		public void AddShow(Show show)
 		{
-            this.shows.Add(show);
+			shows.Add(show);
 		}
 
 		public void PrintShows()
 		{
-
-            foreach (var show in shows)
-            {
-                Console.WriteLine(show);
-            }
-        }
+			foreach (var show in shows)
+			{
+				Console.WriteLine(show);
+			}
+		}
 
 		public void PrintShows(Genre genre)
 		{
-            foreach (var show in shows)
-            {
-                if (show.Movie.Genre.HasFlag(genre))
-                {
-                    Console.WriteLine(show);
-                }
-
-            }
-
-          
+			foreach (var show in shows)
+			{
+				if (show.Movie.Genre.HasFlag(genre))
+				{
+					Console.WriteLine(show);
+				}
+			}
 		}
 
 		public void PrintShows(Day day)
 		{
-
-            var Shows = shows.FindAll(d => d.Day == day);
-            foreach (var show in Shows)
-            {
-               
-                Console.WriteLine(show);
-            }
-        }
+			var Shows = shows.FindAll(d => d.Day == day);
+			foreach (var show in Shows)
+			{
+				Console.WriteLine(show);
+			}
+		}
 
 		public void PrintShows(Time time)
 		{
-            var Shows = shows.FindAll(t => t.Time == time);
-            foreach (var show in Shows)
-            {
-
-                Console.WriteLine(show);
-            }
-
-        }
+			var Shows = shows.FindAll(t => t.Time == time);
+			foreach (var show in Shows)
+			{
+				Console.WriteLine(show);
+			}
+		}
 
 		public void PrintShows(string actor)
 		{
-            var Shows = shows.FindAll(a => a.Movie.Cast.Contains(actor));
-            foreach (var show in Shows)
-            {
-
-                Console.WriteLine(show);
-            }
-
-        }
+			var Shows = shows.FindAll(a => a.Movie.Cast.Contains(actor));
+			foreach (var show in Shows)
+			{
+				Console.WriteLine(show);
+			}
+		}
 
 		public void PrintShows(Day day, Time time)
 		{
-            var Shows = shows.FindAll(s => s.Time == time && s.Day == day);
-            foreach (var show in Shows)
-            {
-
-                Console.WriteLine(show);
-            }
-
-        }
+			var Shows = shows.FindAll(s => s.Time == time && s.Day == day);
+			foreach (var show in Shows)
+			{
+				Console.WriteLine(show);
+			}
+		}
 	}
 }
